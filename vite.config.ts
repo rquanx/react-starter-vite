9e0be0c2d415bdfd95d77b55c8cf29b2164dc7af
runquan.tan <runquan.tan@medalsoft.com>
2021-04-28 15:23:25 +0800

    chore: vite config move require to import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-const path = require("path");
-const Path = require("./scripts/path");
+import path from "path";
+import Path from "./scripts/path";
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
